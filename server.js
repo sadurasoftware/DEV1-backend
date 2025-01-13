ffdcdc913da0aad987fc676b4f72225eceda94ac
Deepika140101 <deepika@sadurasoftware.com>
2025-01-13 16:49:08 +0530

    Login_Logout

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require("./routes/authRoutes.js")
-const port = 5000;
 
 // Create Express app
 const app = express();
@@ -20,7 +19,7 @@ app.use('/api', userRoutes);
 app.use('/api/auth',authRoutes)
 
 // Set up server to listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
