04b5bd0c392a158621c86940e8a424eeb6f19be2
Deepika140101 <deepika@sadurasoftware.com>
2025-02-03 17:31:05 +0530

    permission

--- models/Permission.js
+++ models/Permission.js
@@ -4,7 +4,6 @@ const sequelize = require("../config/database");
 const Permission=sequelize.define("Permission",{
     id: {type: DataTypes.INTEGER,primaryKey: true,autoIncrement: true,},
     name:{type:DataTypes.STRING,allowNull:false,unique:true},
-    description:{type:DataTypes.STRING,allowNull:true},
 });
 
 module.exports=Permission;
