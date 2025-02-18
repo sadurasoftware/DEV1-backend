31e58a5b4cbba8cd73972f8c33e0b8af3924897c
vigneshwari <vigneshwari@sadurasoftware.com>
2025-02-18 17:14:14 +0530

    Modules,permissions edited

--- routes/moduleRoutes.js
+++ routes/moduleRoutes.js
@@ -6,5 +6,7 @@ const validator=require('../validator/router-validator');
 router.post('/create',validator.moduleValidator,ModuleController.createModule);
 router.get('/get', ModuleController.getModule);
 router.get('/get/:id', ModuleController.getModuleById);
+router.put('/update/:id',ModuleController.updateModule);
+router.delete('/delete/:id',ModuleController.deleteModule);
 
 module.exports=router;
