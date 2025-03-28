c3a6e63b1237ca9c327b6eef3a0a54513cba3ae9
Deepika140101 <deepika@sadurasoftware.com>
2025-03-28 10:39:13 +0530

    Category

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize('dev-db', 'admin', 'awsSadura#db123', {
-//   host: 'dev-db.cvi4kce4mqlf.us-east-1.rds.amazonaws.com',
-//   dialect: 'mysql',
-//   logging: console.log, 
-// });
-
-// module.exports = sequelize;
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('dev-db', 'root', 'root@123', {
-  host: 'localhost',
+const sequelize = new Sequelize('dev_db', 'admin', 'awsSadura#db123', {
+  host: 'node-dev.cvi4kce4mqlf.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   logging: console.log, 
 });
 
 module.exports = sequelize;
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize('dev-db', 'root', 'root@123', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   logging: console.log, 
+// });
+
+// module.exports = sequelize;
