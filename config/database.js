0cac19eec5b7d6c401968a1275d082e998702f18
BalaK <balak@sadurasoftware.com>
2025-03-31 11:08:16 +0530

    add vercel

--- config/database.js
+++ config/database.js
@@ -1,20 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('dev_db', 'admin', 'awsSadura#db123', {
-  host: 'node-dev.cvi4kce4mqlf.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   logging: console.log, 
 });
 
 module.exports = sequelize;
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize('dev-db', 'root', 'root@123', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   logging: console.log, 
-// });
-
-// module.exports = sequelize;
